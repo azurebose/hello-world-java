94a089f41d0a947d07f8cb2497b2d9fbec0fefd8
Milky19 <97377003+Milky19@users.noreply.github.com>
2022-08-18 10:19:42 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -42,7 +42,7 @@ public class HelloWorld {
     System.out.println("Hello, World");
     System.out.println("Hello, World");
     System.out.println("Hello, World");
-    System.out.println("Hello, World");
+    System.out.println("something");
     System.out.println("Hello, World");
     System.out.println("Hello, World");
     System.out.println("Hello, World");
